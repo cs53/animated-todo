ed8ed687fb42d8fc1b7724ad04134672b3dc747f
MohamadTarshishi <51304061+cs53@users.noreply.github.com>
2022-10-27 20:39:28 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AppContainer from './src/components/app-container'
+import Navigator from './src/'
+// import reactimport { gestureHandlerRootHOC } from 'react-native-gesture-handler'
+// -native-gestureHandlerRootHO
+
+export default function App() {
+  return (
+    <AppContainer>
+      <Navigator />
+    </AppContainer>
+  )
+}
